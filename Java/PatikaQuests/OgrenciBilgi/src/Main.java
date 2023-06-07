cc083cb2e1a87743ce2d17fa6f95f53a3c594c85
Kürşat Batuhan Aslan <kursataslanb@gmail.com>
2023-06-07 15:58:21 +0300

    aa

---
+++ Java/PatikaQuests/OgrenciBilgi/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Course mat = new Course("Matematik", "MAT101", "MAT");
+        Course fizik = new Course("Fizik", "FZK101", "FZK");
+        Course kimya = new Course("Kimya", "KMY101", "KMY");
+
+        Teacher t1 = new Teacher("T1", "90550000000", "MAT");
+        Teacher t2 = new Teacher("T2", "90550000001", "FZK");
+        Teacher t3 = new Teacher("T3", "90550000002", "KMY");
+
+        mat.addTeacher(t1);
+        fizik.addTeacher(t2);
+        kimya.addTeacher(t3);
+
+        Student s1 = new Student("Ogr1", 4, "140144015", mat, fizik, kimya);
+        s1.addBulkExamNote(50,20,40);
+        s1.isPass();
+
+        Student s2 = new Student("Ogr2", 4, "2211133", mat, fizik, kimya);
+        s2.addBulkExamNote(100,50,40);
+        s2.isPass();
+
+        Student s3 = new Student("Ogr3", 4, "221121312", mat, fizik, kimya);
+        s3.addBulkExamNote(50,20,40);
+        s3.isPass();
+
+    }
+}
